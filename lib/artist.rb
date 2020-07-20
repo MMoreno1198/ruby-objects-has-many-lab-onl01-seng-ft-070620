2f5b42369541b08ef4b21eb3a26b378687bad051
Marcus Moreno <legendary.1198@gmail.com>
2020-07-20 20:35:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,47 +1,32 @@
-class Artist 
-  
+class Artist
   attr_accessor :name
-  
+
   @@all = []
-  
-  def artist(name)
-    
+
+  def initialize(name)
     @name = name
-    
-    @@all << self 
-    
-  end 
-  
-  def self.all
-    
-    @@all 
-    
-  end 
-  
-  def new_song(song)
-  
-    song.artist = self
-    
+    @@all << self
   end
 
-def add_song_by_name(name)
-  
-  song = Song.new(name)
-  
-  song.artist = self
-  
+  def self.all
+    @@all
   end
-  
-  def songs 
-    
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
     Song.all.select {|song| song.artist == self}
-    
   end
-  
+
   def self.song_count
-    
     Song.all.count
-    
   end
 end
     
